8cf3293d47c01cdfcf7eeb2d9d04d5c6af5f903e
dacostacds <dacostacds@gmail.com>
2024-04-02 16:29:43 -0300

    adding allure report config

--- config/wdio.shared.conf.js
+++ config/wdio.shared.conf.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const allure = require('allure-commandline')
 
 exports.config = {
     //
@@ -119,12 +120,12 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    
+
     //reporters: ['spec'],
-    reporters: [['allure', {
+    reporters: ['spec',['allure', {
         outputDir: 'allure-results',
-        disableWebdriverStepsReporting: true,
-        disableWebdriverScreenshotsReporting: true,
+        disableWebdriverStepsReporting: false,
+        disableWebdriverScreenshotsReporting: false,
     }]],
     
     //
@@ -228,9 +229,12 @@ exports.config = {
      * @param {number}  result.duration  duration of test
      * @param {boolean} result.passed    true if test has passed, otherwise false
      * @param {object}  result.retries   informations to spec related retries, e.g. `{ attempts: 0, limit: 0 }`
-     */
-    // afterTest: function(test, context, { error, result, duration, passed, retries }) {
-    // },
+     */,//async function(test, context, { error })
+    afterTest: async function(test, context, { error }) {
+        if (error) {
+            await browser.takeScreenshot();
+        }
+    },
 
 
     /**
@@ -272,9 +276,27 @@ exports.config = {
      * @param {object} config wdio configuration object
      * @param {Array.<Object>} capabilities list of capabilities details
      * @param {<Object>} results object containing test results
-     */
-    // onComplete: function(exitCode, config, capabilities, results) {
-    // },
+     *///function(exitCode, config, capabilities, results)
+    onComplete: function() {
+        const reportError = new Error('Could not generate Allure report')
+        const generation = allure(['generate', 'allure-results', '--clean'])
+        return new Promise((resolve, reject) => {
+            const generationTimeout = setTimeout(
+                () => reject(reportError),
+                5000)
+
+            generation.on('exit', function(exitCode) {
+                clearTimeout(generationTimeout)
+
+                if (exitCode !== 0) {
+                    return reject(reportError)
+                }
+
+                console.log('Allure report successfully generated')
+                resolve()
+            })
+        })
+    },
     /**
     * Gets executed when a refresh happens.
     * @param {string} oldSessionId session ID of the old session
